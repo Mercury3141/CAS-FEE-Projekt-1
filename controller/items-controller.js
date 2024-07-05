1a72496380085ee160d1c2790ffb7a7e27945aa6
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-07-05 17:07:52 +0200

    Changes to index-controller

--- controller/items-controller.js
+++ controller/items-controller.js
@@ -1,19 +1,13 @@
 import {itemStore} from '../services/item-store.js'
-
-// import {SecurityUtil} from '../utils/security.js'
+import {groupStore} from '../services/group-store.js'
 
 export class ItemController {
-
     getItems = async (req, res) => {
-        res.json((await itemStore.all() || []));
+        res.json(await itemStore.all() || []);
     };
 
     createItem = async (req, res) => {
         res.json(await itemStore.add(req.body.name));
-    };
-
-    showItem = async (req, res) => {
-        res.json(await itemStore.get(req.params.id));
     };
 
     deleteItem = async (req, res) => {
@@ -25,25 +19,22 @@ export class ItemController {
     };
 
 
+    getGroups = async (req, res) => {
+        res.json(await groupStore.getItems(req.params.groupId));
+    };
+
     createGroup = async (req, res) => {
         res.json(await groupStore.add(req.body.name));
     };
 
-    addItemToGroup = async (req, res) => {
-        res.json(await groupStore.addItem(req.params.groupId, req.body.itemId));
-    };
-
-    removeItemFromGroup = async (req, res) => {
-        res.json(await groupStore.removeItem(req.params.groupId, req.params.itemId));
-    };
-
-    getGroupItems = async (req, res) => {
-        res.json(await groupStore.getItems(req.params.groupId));
-    };
-
     deleteGroup = async (req, res) => {
         const result = await groupStore.delete(req.params.groupId);
+        res.json(result);
+    };
 
+    updateGroup = async (req, res) => {
+        res.json(await groupStore.update(req.params.id, req.body.name));
+    };
+}
 
-        export const itemController = new ItemController();
-    }
+export const itemController = new ItemController();
