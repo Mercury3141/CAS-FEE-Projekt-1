908d4d39f1c3963f4c540df53d59376f9ce31cd5
Mercury3141 <66523675+Mercury3141@users.noreply.github.com>
2024-07-09 20:56:46 +0200

    Distinct html scripts.

--- public/js/controllers/index-controller.js
+++ public/js/controllers/index-controller.js
@@ -14,34 +14,29 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         const groupTemplate = document.getElementById('group-template').innerHTML;
-        const compiledTemplate = Handlebars.compile(groupTemplate);
-        const groupHTML = compiledTemplate(context);
+        const compiledGroupTemplate = Handlebars.compile(groupTemplate);
+        const groupHTML = compiledGroupTemplate(context);
 
         const groupList = document.getElementById('group-list');
         groupList.insertAdjacentHTML('beforeend', groupHTML);
-    }
 
-    groupContainer.addEventListener('click', (event) => {
-        if (event.target && event.target.id === 'add-item') {
-            const groupId = event.target.getAttribute('data-group');
-            addItemToGroup(groupId);
-        }
-    });
+        const addItemButton = document.getElementById(`add-item-${groupId}`);
+        addItemButton.addEventListener('click', () => addItemToGroup(groupId));
+    }
 
     function addItemToGroup(groupId) {
         const groupElement = document.getElementById(`group-${groupId}`);
-        const itemList = groupElement.querySelector(`ul`);
+        const itemList = groupElement.querySelector(`#item-list-${groupId}`);
         const itemId = Date.now(); // Unique ID for the item
 
-        const itemHTML = `
-            <li class="item" aria-labelledby="item-name-input-${groupId}-${itemId}" data-id="${itemId}" data-group="${groupId}">
-                <input type="checkbox" id="item-checkbox-${groupId}-${itemId}" title="Toggle Completed" aria-label="Toggle New Reminder" tabindex="0" data-id="${itemId}">
-                <input class="label-item" id="reminder-item-${groupId}-${itemId}" type="text" placeholder="New Reminder" title="Enter Reminder Description" aria-label="New Reminder" data-id="${itemId}">
-                <button class="margins-left-right" id="toggle-important" title="Toggle Importance" aria-pressed="false" aria-label="Toggle Importance" tabindex="0" data-id="${itemId}">!</button>
-                <input class="date-input" type="date" id="item-date-${groupId}-${itemId}" name="item-date" title="Set Due Date" value="" aria-describedby="due-date-text-${groupId}-${itemId}" data-id="${itemId}">
-            </li>
-            <li class="label-due-date" id="due-date-text-${groupId}-${itemId}" style="display: none;">Due on</li>
-        `;
+        const context = {
+            id: itemId,
+            groupId: groupId
+        };
+
+        const itemTemplate = document.getElementById('item-template').innerHTML;
+        const compiledItemTemplate = Handlebars.compile(itemTemplate);
+        const itemHTML = compiledItemTemplate(context);
 
         itemList.insertAdjacentHTML('beforeend', itemHTML);
     }
